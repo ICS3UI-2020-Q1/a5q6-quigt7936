4f15fe128cd158e18c81d45a0b0dd21ecf4a98d0
quigt7936 <71140817+quigt7936@users.noreply.github.com>
2020-10-07 09:06:57 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Program creates astrics relating to the number given
+ * @author Thomas Quigley
  */
 public class Main {
-
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    // get the users number from 1-10, if the number is't 1-10 then ask again
+    int userNumber = 0;
+    while (userNumber > 10 || userNumber < 1) {
+      System.out.println("Please enter a number between 1 and 10");
+      userNumber = input.nextInt();
+    }
+    
+    // for loop that stops when it reaches 1 less than the number
+    
+    for (int i = 0; i < userNumber; i++) {
+      for (int j = 0; j < userNumber; j++) {
+        // print an astric
+        System.out.print("*");
+      }
+      System.out.println("");
+    }
     
   }
 }
